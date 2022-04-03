11c347c1c97e8b611c9d4473ad384115215e9637
thewhite017 <100134033+thewhite017@users.noreply.github.com>
2022-04-03 09:35:39 +0700

    Update 23-Tetris.c

--- Praktikum 6/23-Tetris.c
+++ Praktikum 6/23-Tetris.c
@@ -1,5 +1,5 @@
 /*Nama File     : Tetris.c*/
-/*Deskripsi     : Menampilkan susunan karakter * sesuai dengan susunan. Tinggi berdasarkan N > 0*/
+/*Deskripsi     : menampilkan susunan karakter * sesuai dengan susunan. Tinggi berdasarkan N > 0*/
 /*Pembuat       : Daffa Aulia Rizky Karunia - 24060121130070*/
 /*Tgl Pembuatan : Minggu, 03 04 2022 09:17 WIB*/
 
@@ -20,11 +20,16 @@ int main()
    printf("Masukkan jumlah baris : ");
    scanf("%d", &N);
    printf("\n");
-   for(i=1; i<=N; i++){        //Baris
-       for(j=1; j<=i; j++){    //Kolom
-            printf("*");
+   if (N <= 0){
+        printf("N harus integer positif\n");
+    }
+    else{
+        for(i=1; i<=N; i++){        //Baris
+            for(j=1; j<=i; j++){    //Kolom
+                printf("*");
+            }
+        printf("\n");
         }
-       printf("\n");
     }
    return 0;
 }
